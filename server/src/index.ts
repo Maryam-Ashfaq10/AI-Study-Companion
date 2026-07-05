c7016d28630dc58a9f20710ab6f239245eccfc50
Maryam-Ashfaq10 <maryammashfaq000@gmail.com>
2026-07-05 17:36:54 +0500

    updated index file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.get('/health', (req,res) => {
+    res.json('running...')
+})
+
+const port = process.env.PORT
+
+app.listen(port, () => {
+    console.log('server listening on port', port)
+})
